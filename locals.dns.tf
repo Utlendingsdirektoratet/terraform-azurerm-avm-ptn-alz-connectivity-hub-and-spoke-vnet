16c9b9a2fb10ec93d90e9cbf973d66f5130408bb
Morten Lerudjordet <github@lerun.info>
2025-09-18 16:47:58 +0200

    simplify

--- locals.dns.tf
+++ locals.dns.tf
@@ -3,14 +3,14 @@ locals {
 }
 
 locals {
- private_dns_zones = { for key, value in var.hub_virtual_networks : key => merge({
+  private_dns_zones = { for key, value in var.hub_virtual_networks : key => merge({
     location            = value.hub_virtual_network.location
     resource_group_name = value.hub_virtual_network.resource_group_name
   }, value.private_dns_zones.dns_zones) if local.private_dns_zones_enabled[key] }
   private_dns_zones_auto_registration = { for key, value in var.hub_virtual_networks : key => {
-    location            = value.hub_virtual_network.location
-    domain_name         = value.private_dns_zones.auto_registration_zone_name
-    parent_id           = try(value.private_dns_zones.dns_zones.parent_id, provider::azapi::subscription_resource_id(data.azapi_client_config.current.subscription_id, local.resource_group_resource_type, [local.private_dns_zones[key].resource_group_name]))
+    location    = value.hub_virtual_network.location
+    domain_name = value.private_dns_zones.auto_registration_zone_name
+    parent_id   = provider::azapi::subscription_resource_id(data.azapi_client_config.current.subscription_id, local.resource_group_resource_type, [local.private_dns_zones[key].resource_group_name])
     virtual_network_links = {
       auto_registration = {
         vnetlinkname     = "vnet-link-${key}-auto-registration"

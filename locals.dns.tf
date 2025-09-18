37e1112ba97f212ab2fe74197bf2014697b38975
Morten Lerudjordet <moler@udi.no>
2025-09-18 15:27:42 +0200

    Update locals.dns.tf

--- locals.dns.tf
+++ locals.dns.tf
@@ -5,7 +5,7 @@ locals {
 locals {
  private_dns_zones = { for key, value in var.hub_virtual_networks : key => merge({
     location            = value.hub_virtual_network.location
-    parent_id           = provider::azapi::subscription_resource_id(data.azapi_client_config.current.subscription_id, local.resource_group_resource_type, [value.hub_virtual_network.resource_group_name])
+    resource_group_name = value.hub_virtual_network.resource_group_name
   }, value.private_dns_zones.dns_zones) if local.private_dns_zones_enabled[key] }
   private_dns_zones_auto_registration = { for key, value in var.hub_virtual_networks : key => {
     location            = value.hub_virtual_network.location

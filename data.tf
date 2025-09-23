8c5a0eb88cfd4b1590db3b40fbb534611cb7174c
Morten Lerudjordet <github@lerun.info>
2025-09-23 08:14:58 +0200

    comment

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-# Randomly gives wrong sub id
+# Does not honor provider overrides set on module level
 # https://github.com/Azure/terraform-provider-azapi/issues/963
 data "azapi_client_config" "current" {}
 data "azurerm_subscription" "current" {}

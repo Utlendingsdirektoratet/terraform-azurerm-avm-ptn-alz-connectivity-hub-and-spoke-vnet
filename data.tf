12994e39022121048e4d07245d766e8bb1cf91bf
Morten Lerudjordet <github@lerun.info>
2025-09-24 13:05:44 +0200

    revert

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
 # Does not honor provider overrides set on module level
 # https://github.com/Azure/terraform-provider-azapi/issues/963
 data "azapi_client_config" "current" {}
-data "azurerm_subscription" "current" {}
+# data "azurerm_subscription" "current" {}
--- locals.gateways.tf
+++ locals.gateways.tf
@@ -5,7 +5,7 @@ locals {
     for hub_network_key, hub_network_value in var.hub_virtual_networks : "${hub_network_key}-express-route" => {
       virtual_network_gateway_subnet_id = module.hub_and_spoke_vnet.virtual_networks[hub_network_key].subnets["${hub_network_key}-gateway"].resource_id
       virtual_network_gateway = merge({
-        parent_id       = try(hub_network_value.virtual_network_gateways.express_route.parent_id, provider::azapi::subscription_resource_id(data.azurerm_subscription.current.subscription_id, local.resource_group_resource_type, [hub_network_value.hub_virtual_network.resource_group_name]))
+        parent_id       = try(hub_network_value.virtual_network_gateways.express_route.parent_id, provider::azapi::subscription_resource_id(data.azapi_client_config.current.subscription_id, local.resource_group_resource_type, [hub_network_value.hub_virtual_network.resource_group_name]))
         hub_network_key = hub_network_key
         type            = "ExpressRoute"
       }, hub_network_value.virtual_network_gateways.express_route)
@@ -18,7 +18,7 @@ locals {
     for hub_network_key, hub_network_value in var.hub_virtual_networks : "${hub_network_key}-vpn" => {
       virtual_network_gateway_subnet_id = module.hub_and_spoke_vnet.virtual_networks[hub_network_key].subnets["${hub_network_key}-gateway"].resource_id
       virtual_network_gateway = merge({
-        parent_id       = try(hub_network_value.virtual_network_gateways.vpn.parent_id, provider::azapi::subscription_resource_id(data.azurerm_subscription.current.subscription_id, local.resource_group_resource_type, [hub_network_value.hub_virtual_network.resource_group_name]))
+        parent_id       = try(hub_network_value.virtual_network_gateways.vpn.parent_id, provider::azapi::subscription_resource_id(data.azapi_client_config.current.subscription_id, local.resource_group_resource_type, [hub_network_value.hub_virtual_network.resource_group_name]))
         hub_network_key = hub_network_key
         type            = "Vpn"
       }, hub_network_value.virtual_network_gateways.vpn)

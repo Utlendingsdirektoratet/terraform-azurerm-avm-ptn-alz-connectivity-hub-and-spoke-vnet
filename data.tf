e715934fab8d687a9eb9355391282e3f4dd7d811
Morten Lerudjordet <moler@udi.no>
2025-09-26 13:16:53 +0200

    Update data.tf
    Bug fixed

--- data.tf
+++ data.tf
@@ -1,4 +1,1 @@
-# Does not honor provider overrides set on module level
-# https://github.com/Azure/terraform-provider-azapi/issues/963
 data "azapi_client_config" "current" {}
-# data "azurerm_subscription" "current" {}
